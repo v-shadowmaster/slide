7e954f39bbdbf512e979e2b0cfaece47902f02e2
Vinay Kumar <vinayp.21.beis@acharya.ac.in>
2026-04-11 02:50:49 +0530

    updated

---
+++ src/app/(protected)/payment/loading.tsx
@@ -0,0 +1,11 @@
+import Loader from '@/components/global';
+
+const Loading = () => {
+  return (
+    <div>
+      <Loader state>...loading</Loader>
+    </div>
+  );
+};
+
+export default Loading;
